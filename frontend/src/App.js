ecd8032ff2d4f7d5c161b744156e98b6f77d9ee6
Mac <ronald890119@gmail.com>
2024-07-08 00:44:12 +1000

    Add button to load data from database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,48 @@
 import styles from "./App.module.css";
 import HomePage from './pages/HomePage';
-import { useEffect, useState } from "react";
 import axios from "axios";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { loadAsync, remove, add } from "./state/uniSlice";
 
 function App() {
-  // variable to store API data
-  const [uniData, setUniData] = useState([]);
+  // it is used for updating state
   const dispatch = useDispatch();
 
-  // load university data at the beginning
-  useEffect(() => {
+  // load university data from database
+  const loadFromDB = () => {
     axios
-      .get("http://universities.hipolabs.com/search?country=Australia")
+      .get("http://localhost:5000/uni/load")
       .then((res) => {
-        setUniData(res.data);
+        dispatch(loadAsync(res.data))
       })
       .catch((err) => {
         console.log(err);
       })
-  }, []);
+  };
+
+  // load university data from API
+  const loadFromAPI = () => {
+    axios
+      .get("http://universities.hipolabs.com/search?country=Australia")
+      .then((res) => {
+        dispatch(loadAsync(res.data))
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  };
 
   return (
     <div className={styles.App}>
       <HomePage/>
       <div className={styles.buttonContainer}>
         <Stack spacing={{ xs: 3, sm: 2 }} direction="row" justifyContent="center" useFlexGap flexWrap="wrap">
-          <Button variant="load" style={{backgroundColor: '#95b222'}} onClick={() => dispatch(loadAsync(uniData))}>Load</Button>
+          <Button variant="load" style={{backgroundColor: '#95b222'}} onClick={loadFromAPI}>Load</Button>
           <Button variant="delete" style={{backgroundColor: '#95b222'}} onClick={() => dispatch(remove())}>Delete</Button>
           <Button variant="add" style={{backgroundColor: '#95b222'}} onClick={() => dispatch(add())}>Add</Button>
-          <Button variant="database" style={{backgroundColor: '#95b222'}}>Load from Database</Button>
+          <Button variant="database" style={{backgroundColor: '#95b222'}} onClick={loadFromDB}>Load from Database</Button>
         </Stack>
       </div>
     </div>
